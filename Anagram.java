a1d868289710160de4130306edb06efc624c0c0e
Durgesh Gangaram Deore <88814738+DurgeshDeore@users.noreply.github.com>
2024-10-30 13:33:39 +0530

    Create Anagram.java
    GFG
    Easy
    Given two strings s1 and s2 consisting of lowercase characters. The task is to check whether two given strings are an anagram of each other or not. An anagram of a string is another string that contains the same characters, only the order of characters can be different. For example, act and tac are an anagram of each other. Strings s1 and s2 can only contain lowercase alphabets.
    
    Note: You can assume both the strings s1 & s2 are non-empty.

---
+++ Anagram.java
@@ -0,0 +1,24 @@
+class Solution {
+    // Function is to check whether two strings are anagram of each other or not.
+    public static boolean areAnagrams(String s1, String s2) {
+        if(s1.length() != s2.length()){ //if length are differrent the return false
+            return false;
+        }
+        int n = s1.length();
+        int a1[] = new int[26];
+        int a2[] = new int[26];
+        int indx1 = 0,indx2 = 0;
+        for(int i=0; i<n;i++){ //for loop for counting elements & their frequency
+            indx1 = s1.charAt(i)-97;
+            indx2 = s2.charAt(i)-97;
+            a1[indx1] += 1;
+            a2[indx2] += 1;
+        }
+        for(int i=0; i<26  ;i++){ //for loop to check both array contains same elements or not
+            if(a1[i] != a2[i]){
+                return false; //return false if both contains defferent elements
+            }
+        }
+        return true; 
+    }
+}
