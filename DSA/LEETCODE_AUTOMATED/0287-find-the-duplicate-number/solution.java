182965960ab2e258e33b54eb17868a41107612dd
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2024-12-04 12:46:02 +0000

    [LeetCode Sync] Runtime - 4 ms (92.34%), Memory - 59.9 MB (99.95%)

---
+++ DSA/LEETCODE_AUTOMATED/0287-find-the-duplicate-number/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        if(nums.length <= 1){
+            return -1;
+        }
+        int freq[] = new int[nums.length];
+        for(int ele: nums){
+            freq[ele]++;
+        }
+        int indx=0;
+        for(int ele: freq){
+            if(ele > 1){
+                return indx;
+            }
+            indx++;
+        }
+        return -1;
+    }
+}
