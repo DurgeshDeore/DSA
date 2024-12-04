faa95056e79ed6da0f6e80a5d66e1d23d9ac7c64
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2024-12-04 12:32:33 +0000

    [LeetCode Sync] Runtime - 7 ms (57.84%), Memory - 45.8 MB (99.94%)

---
+++ DSA/LEETCODE_AUTOMATED/0387-first-unique-character-in-a-string/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int charCnt[] = new int[26];
+        if(s.length()==0){
+            return 0;
+        }
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            charCnt[ch-'a']++;
+        }
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(charCnt[ch-'a'] == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
