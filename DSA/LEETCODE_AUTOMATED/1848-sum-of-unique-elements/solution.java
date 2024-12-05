c06d2e77d9dba2efca864a7ff83159b19b20e12a
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2024-12-05 12:00:31 +0000

    [LeetCode Sync] Runtime - 2 ms (57.28%), Memory - 41.9 MB (99.96%)

---
+++ DSA/LEETCODE_AUTOMATED/1848-sum-of-unique-elements/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int sum=0;
+        for(int ele: nums){
+            map.put(ele,map.getOrDefault(ele,0)+1);
+        }
+        for(int ele: map.keySet()){
+            if(map.get(ele)==1){
+                sum+=ele;
+            }
+        }
+        return sum;
+    }
+}
