1891ad685ece7dbc84f7612e1e2c3b32938273ec
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2024-12-09 10:32:38 +0000

    [LeetCode Sync] Runtime - 20 ms (5.06%), Memory - 58.2 MB (99.98%)

---
+++ DSA/LEETCODE_AUTOMATED/0234-palindrome-linked-list/solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        StringBuffer tmp = new StringBuffer("");
+        ListNode temp = head;
+        while(temp!=null){
+            tmp.append(temp.val);
+            temp=temp.next;
+        }
+        String str = tmp.toString();
+        String rev = new StringBuilder(tmp).reverse().toString();
+        if(rev.equals(str)){
+            return true;
+        }
+        return false;
+    }
+}
