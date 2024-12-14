c12187eb05087660cacb3836cc5ce7b1559fff4d
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2024-12-14 22:09:48 +0530

    Create SearchInRotatedSortedArray.java
    GFG
    Medium

---
+++ SearchInRotatedSortedArray.java
@@ -0,0 +1,18 @@
+//method = two pointers
+class Solution {
+    int search(int[] arr, int key) {
+        int l=0, r=arr.length-1;
+        while(l <= r){
+            if(arr[l]==key){
+                return l;
+            }else if(arr[r]==key){
+                return r;
+            }else if(arr[l] < key){
+                l++;
+            }else{
+                r--;
+            }
+        }
+        return -1;
+    }
+}
