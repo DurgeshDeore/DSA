4460b02be78b878f752b7c5df2c4e4cbfc0ae44e
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2024-12-15 04:25:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45.5 MB (99.92%)

---
+++ DSA/LEETCODE_AUTOMATED/0268-missing-number/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int a_sum=n*(n+1)/2, sum=0;
+        for(int ele: nums){
+            sum+=ele;
+        }
+        return a_sum-sum;
+    }
+}
