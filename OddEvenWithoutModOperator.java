a4855732c6f6ed7348bb6e2dba91f00d52835b20
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2024-12-17 19:32:35 +0530

    Create OddEvenWithoutModOperator.java
    Interview Question

---
+++ OddEvenWithoutModOperator.java
@@ -0,0 +1,19 @@
+//method = filp the answer
+//package extra;
+
+public class OddEven {
+    public static boolean isEvenOrOdd(int n){
+        boolean ans = true;
+        for(int i=0;i<=n; i++){ //T O(n)
+            ans = (!ans);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        if(isEvenOrOdd(2)){
+            System.out.println("Odd");
+        }else{
+            System.out.println("Even");
+        }
+    }
+}
