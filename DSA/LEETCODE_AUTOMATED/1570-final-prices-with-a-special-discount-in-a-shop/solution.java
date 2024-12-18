b53d8ad5bdd02ab70373a88812939a2f914d1fc1
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2024-12-18 02:07:17 +0000

    [LeetCode Sync] Runtime - 2 ms (70.95%), Memory - 43.7 MB (99.93%)

---
+++ DSA/LEETCODE_AUTOMATED/1570-final-prices-with-a-special-discount-in-a-shop/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length-1;
+        for(int i=0; i<n+1; i++){
+            int dis=0;
+            for(int j=i+1; j<prices.length;j++){
+                if(prices[i] >= prices[j]){
+                    prices[i] = prices[i] - prices[j];
+                    dis=prices[j];
+                    break;
+                }
+                prices[i] = prices[i] - dis;
+            }
+        }
+        return prices;
+    }
+}
