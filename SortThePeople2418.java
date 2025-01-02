f99bcff04a6736b60f9148eaf3155e0f00ddb05b
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-01-02 19:45:48 +0530

    Create SortThePeople2418.java
    LeetCode
    Easy

---
+++ SortThePeople2418.java
@@ -0,0 +1,18 @@
+//method = hashing
+//best approach
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        int i=0;
+        HashMap<Integer,String> map= new HashMap<>();
+        String[] res= new String[names.length];
+        for(String name: names){
+            map.put(heights[i++],name);
+        }
+        int j=0;
+        Arrays.sort(heights);
+        for(i=names.length-1; i>=0; i--){
+            res[j++]=map.get(heights[i]);
+        }
+        return res;
+    }
+}
