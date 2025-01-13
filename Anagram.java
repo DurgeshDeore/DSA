ab065a885e4cd7894ac2eaa3a31a6c597f8e5f94
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-01-13 22:53:50 +0530

    Update Anagram.java
    GFG
    easy
    optimised solution

--- Anagram.java
+++ Anagram.java
@@ -1,25 +1,22 @@
 //method = freq[] arr
+//optimized appraoch
 class Solution {
-    // Function is to check whether two strings are anagram of each other or not.
     public static boolean areAnagrams(String s1, String s2) {
-        if(s1.length() != s2.length()){ //if length are differrent the return false
+        if(s1.length() != s2.length()){
             return false;
         }
-        int n = s1.length();
-        int a1[] = new int[26];
-        int a2[] = new int[26];
-        int indx1 = 0,indx2 = 0;
-        for(int i=0; i<n;i++){ //for loop for counting elements & their frequency
-            indx1 = s1.charAt(i)-97;
-            indx2 = s2.charAt(i)-97;
-            a1[indx1] += 1;
-            a2[indx2] += 1;
+        int[] freq= new int[26];
+        for(int i=0; i<s1.length(); i++){
+            freq[s1.charAt(i)-'a']++;
         }
-        for(int i=0; i<26  ;i++){ //for loop to check both array contains same elements or not
-            if(a1[i] != a2[i]){
-                return false; //return false if both contains defferent elements
+        for(int i=0; i<s2.length(); i++){
+            freq[s2.charAt(i)-'a']--;
+        }
+        for(int ele: freq){
+            if(ele != 0){
+                return false;
             }
         }
-        return true; 
+        return true;
     }
 }
