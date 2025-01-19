31b3216853bd44ec381ad80a88d2a9cddf3cd391
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-01-19 09:10:50 +0530

    Create StringsRotationsOfEachOther.java
    GFG
    Easy

---
+++ StringsRotationsOfEachOther.java
@@ -0,0 +1,11 @@
+//best appraoch
+class Solution {
+    // Function to check if two strings are rotations of each other or not.
+    public static boolean areRotations(String s1, String s2) {
+        if(s1.length() != s2.length()){
+            return false;
+        }
+        String temp = s1+s2;
+        return temp.contains(s2);
+    }
+}
