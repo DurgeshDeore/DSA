2cf5a6ab150c4d68c74ef417320092ba64fc465b
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-01-25 08:21:04 +0530

    Create SingleAmongDoubleinSorted.java
    GFG
    Medium

---
+++ SingleAmongDoubleinSorted.java
@@ -0,0 +1,13 @@
+//method = hasing
+class Solution {
+    int findOnce(int[] arr) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int ele: arr){
+            map.put(ele,map.getOrDefault(ele,0)+1);
+        }
+        for(int key: map.keySet()){
+          if(map.get(key)==1) return key;
+        }
+        return -1;
+    }
+}
