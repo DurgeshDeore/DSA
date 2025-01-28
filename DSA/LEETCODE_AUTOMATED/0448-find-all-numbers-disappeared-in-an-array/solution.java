8f62e86fb26c4e5a5177cf19b49ec8817cd0d6b2
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-01-28 18:11:23 +0000

    [LeetCode Sync] Runtime - 6 ms (84.55%), Memory - 56.7 MB (99.99%)

--- DSA/LEETCODE_AUTOMATED/0448-find-all-numbers-disappeared-in-an-array/solution.java
+++ DSA/LEETCODE_AUTOMATED/0448-find-all-numbers-disappeared-in-an-array/solution.java
@@ -3,13 +3,26 @@ class Solution {
         if(nums.length == 0){
             return new ArrayList<>();
         }
+        // better apprroach
         List<Integer> res=new ArrayList<>();
-        int n=1;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int num: nums) map.put(num,1);
-        while(n <= nums.length){
-            if(!map.containsKey(n)) res.add(n);
-            n++;
+
+        // cycle sort
+        // sort
+        int i=0;
+        while(i<nums.length){
+            int curr=nums[i]-1;
+            if(nums[i] != nums[curr]){
+                nums[curr] ^= nums[i];
+                nums[i] ^= nums[curr];
+                nums[curr] ^= nums[i];
+            }else{
+                i++;
+            }
+        }
+        for(i=0;i<nums.length;i++){
+            if(nums[i] != i+1){
+                res.add(i+1);
+            }
         }
         return res;
     }
