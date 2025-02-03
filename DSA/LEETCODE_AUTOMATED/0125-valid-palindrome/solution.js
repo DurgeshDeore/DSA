b29a97877bcdd9cf14284e74dd0e1fd531102d3f
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-02-03 06:12:04 +0000

    [LeetCode Sync] Runtime - 14 ms (8.61%), Memory - 57.5 MB (67.71%)

---
+++ DSA/LEETCODE_AUTOMATED/0125-valid-palindrome/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    let str="";
+    for(let i=0;i<s.length;i++){
+        if(s[i].match(/[a-zA-Z0-9]/)){
+            str+=s[i];
+        }
+    }
+    str=str.toLowerCase();
+    let j=str.length-1,i=0;
+    while(i<j){
+        if(str[i++] !== str[j--]){
+            return false;
+        }
+    }
+    return true;
+};
