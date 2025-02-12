81f99e1d69ee383810fece7fe8cfe80bf1e5cba5
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-02-12 23:16:59 +0530

    Create InsertANodeInBST.java
    Easy

---
+++ GFG/BST/InsertANodeInBST.java
@@ -0,0 +1,12 @@
+class Solution {
+    Node insert(Node root, int Key) {
+        if(root == null) return new Node(Key);
+        if(root.data==Key) return root;
+        if(root.data>Key){
+            root.left=insert(root.left,Key);
+        }else{
+            root.right=insert(root.right,Key);
+        }
+        return root;
+    }
+}
