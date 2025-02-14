a3d6b9459c1b13b6bfb36122585cb47dd431878b
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-02-14 04:36:21 +0000

    [LeetCode Sync] Runtime - 15 ms (100.00%), Memory - 69.6 MB (100.00%)

---
+++ DSA/LEETCODE_AUTOMATED/1477-product-of-the-last-k-numbers/solution.java
@@ -0,0 +1,28 @@
+class ProductOfNumbers {
+    ArrayList<Integer> nums;
+    public ProductOfNumbers() {
+        nums=new ArrayList<>();
+        nums.add(1);
+    }
+    
+    public void add(int num) {
+         if(num==0){
+            nums=new ArrayList<>();
+            nums.add(1);
+            return;
+        }
+        nums.add(num*nums.get(nums.size()-1));
+    }
+    
+    public int getProduct(int k) {
+        if(nums.size()<=k) return 0;
+        return nums.get(nums.size()-1)/nums.get(nums.size()-k-1);
+    }
+}
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers obj = new ProductOfNumbers();
+ * obj.add(num);
+ * int param_2 = obj.getProduct(k);
+ */
