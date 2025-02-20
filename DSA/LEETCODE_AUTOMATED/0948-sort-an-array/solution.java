145e89b198397fbfed934a81c20bbb147edbd3fb
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-02-20 18:32:27 +0000

    [LeetCode Sync] Runtime - 87 ms (9.03%), Memory - 56.6 MB (99.96%)

---
+++ DSA/LEETCODE_AUTOMATED/0948-sort-an-array/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int ele: nums) pq.add(ele);
+        int i=0;
+        while(!pq.isEmpty()) nums[i++]=pq.poll();
+        return nums;
+    }
+}
