41116b485f4ede7db89af41ad06ff7e55c137b70
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-03-03 04:10:03 +0000

    [LeetCode Sync] Runtime - 5 ms (69.21%), Memory - 68.5 MB (99.87%)

---
+++ DSA/LEETCODE_AUTOMATED/2265-partition-array-according-to-given-pivot/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int i=0;
+        int res[]=new int[nums.length];
+        for(int ele: nums){
+            if(ele<pivot) res[i++]=ele;
+        }
+        for(int ele: nums){
+            if(ele==pivot) res[i++]=ele;
+        }
+        for(int ele: nums){
+            if(ele>pivot) res[i++]=ele;
+        }
+        return res;
+    }
+}
