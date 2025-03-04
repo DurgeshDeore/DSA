4cd02ad78d469c7992edc5dcc393b0298eb5ceb9
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-03-04 03:12:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.4 MB (100.00%)

---
+++ DSA/LEETCODE_AUTOMATED/1889-check-if-number-is-a-sum-of-powers-of-three/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkPowersOfThree(int n) {
+        while(n>0) {
+            int rem=n%3;
+            n/=3;
+            if(rem!=1 && rem!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
