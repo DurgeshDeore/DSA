fd1237af00718bf9e9aad83bd479d125ba1ca238
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-03-05 15:56:04 +0000

    [LeetCode Sync] Runtime - 22 ms (5.17%), Memory - 46.1 MB (99.92%)

---
+++ DSA/LEETCODE_AUTOMATED/1019-squares-of-a-sorted-array/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n=nums.length, i=0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        //int[] res=new int[n];
+        for(int ele: nums){
+            pq.add(ele*ele);
+        }
+        while(!pq.isEmpty()) {
+            // res[i]=pq.poll();
+            nums[i++]=pq.poll();
+        }
+        return nums;
+    }
+}
