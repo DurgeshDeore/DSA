b4429f8998fe795b49508cf297c47e2085ac5ff8
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-03-05 02:24:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.1 MB (100.00%)

---
+++ DSA/LEETCODE_AUTOMATED/2649-count-total-number-of-colored-cells/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long coloredCells(int n) {
+        // if(n==1) return n;
+        // long i=4,ans=1;
+        // while(n>1){
+        //     ans+=i;
+        //     i+=4;
+        //     n--;
+        // }
+        // return ans;
+        // formula for the cell n^2 + (n-1)*(n-1)
+        return (long) n*n + (long)(n-1)*(n-1);
+    }
+}
