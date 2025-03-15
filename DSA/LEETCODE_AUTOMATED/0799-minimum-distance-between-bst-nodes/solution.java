e41a6640755e387474216eced1c4c0474c75686d
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-03-15 06:34:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.4 MB (99.96%)

---
+++ DSA/LEETCODE_AUTOMATED/0799-minimum-distance-between-bst-nodes/solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int diff=Integer.MAX_VALUE;
+    TreeNode prev=null;
+    public int minDiffInBST(TreeNode root) {
+        if(root == null) return 0;
+        minDiffInBST(root.left);
+        if(prev!=null){
+            diff=Math.min(diff, root.val-prev.val);
+        }
+        prev=root;
+        minDiffInBST(root.right);
+        return diff;
+    }
+}
