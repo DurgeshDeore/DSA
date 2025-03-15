2923fba0f00f09460f8e472a029d3466aeb904e7
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-03-15 03:48:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.3 MB (99.97%)

---
+++ DSA/LEETCODE_AUTOMATED/1444-number-of-steps-to-reduce-a-number-to-zero/solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    int cnt=0;
+    public int numberOfSteps(int num) {
+        if(num == 0) return cnt;
+        cnt++;
+        return num%2==0? numberOfSteps(num/2): numberOfSteps(num-1);
+    }
+}
