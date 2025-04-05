442820934f095828600af651cb452c75c3f4e81a
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-04-05 02:00:28 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.9 MB (100.00%)

---
+++ DSA/LEETCODE_AUTOMATED/1993-sum-of-all-subset-xor-totals/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    int ans=0;
+    public void helper(int[] nums,int xor, int i){
+        if(i>=nums.length){
+            ans+=xor;
+            return;
+        }
+        // xor+=(xor^=nums[i]);
+        helper(nums,xor^nums[i],i+1);
+        helper(nums,xor,i+1);
+        
+        // helper(nums,xor-=(xor^=nums[i]),i+1);
+    }
+    public int subsetXORSum(int[] nums) {
+        helper(nums,0,0);
+        return ans;
+    }
+}
