7bc97edc6d5e07c70c05aa5caba13d4de2c5a845
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-04-17 16:35:54 +0000

    [LeetCode Sync] Runtime - 1 ms (56.43%), Memory - 42.3 MB (99.88%)

---
+++ DSA/LEETCODE_AUTOMATED/0782-jewels-and-stones/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int cnt=0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char ch: jewels.toCharArray()){
+            // map.put(ch, map.getOrDefalult(ch,0)+1);
+            map.put(ch,1);
+        }
+        for(char ch: stones.toCharArray()){
+            if(map.containsKey(ch)){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
