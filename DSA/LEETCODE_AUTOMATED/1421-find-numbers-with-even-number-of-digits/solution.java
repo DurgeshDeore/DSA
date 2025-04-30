c6c930161f7ab6816cd4accba7744d798835dbf4
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-04-30 02:08:35 +0000

    [LeetCode Sync] Runtime - 1 ms (99.52%), Memory - 43.1 MB (99.99%)

---
+++ DSA/LEETCODE_AUTOMATED/1421-find-numbers-with-even-number-of-digits/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isEven(int num){
+        int cnt=0;
+        while(num != 0){
+            // int ld = num%10;
+            cnt++;
+            num/=10;
+        }
+        return cnt%2==0;
+    }
+    public int findNumbers(int[] nums) {
+        int res = 0;
+        for(int num: nums){
+            if(isEven(num)){
+                res++;
+            }
+        }
+        return res;
+    }
+}
