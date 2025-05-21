16bf0b0ed4f5574a66ce8f1b41b2c66023bdfa39
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-05-21 02:47:24 +0000

    [LeetCode Sync] Runtime - 13 ms (5.93%), Memory - 45.7 MB (99.98%)

--- DSA/LEETCODE_AUTOMATED/0073-set-matrix-zeroes/solution.java
+++ DSA/LEETCODE_AUTOMATED/0073-set-matrix-zeroes/solution.java
@@ -1,21 +1,45 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+        // Map<Integer, Integer> map = new HashMap<>();
+        // int k=-1;
+        // for(int i=0;i<matrix.length;i++){
+        //     for(int j=0;j<matrix[0].length;j++){
+        //         if(matrix[i][j]==0){
+        //             if(map.containsKey(i)) map.put(k--,j);
+        //             else if(map.containsValue(j)) map.put(i,k--);
+        //             else map.put(i,j);
+        //         } 
+        //     }
+        // }
+        // for(int i=0;i<matrix.length;i++){
+        //     for(int j=0;j<matrix[0].length;j++){
+        //         if(map.containsKey(i) || map.containsValue(j)){
+        //             matrix[i][j]=0;
+        //         } 
+        //     }
+        // }
         Map<Integer, Integer> map = new HashMap<>();
-        int k=-1;
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==0){
-                    if(map.containsKey(i)) map.put(k--,j);
-                    else if(map.containsValue(j)) map.put(i,k--);
-                    else map.put(i,j);
-                } 
+        int m = matrix[0].length, n = matrix.length, k=-1;
+        //find the 0
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    if(map.containsKey(i)){
+                        map.put(k--,j);
+                    }else if(map.containsValue(j)){
+                        map.put(i,k--);
+                    }else{
+                        map.put(i,j);
+                    }
+                }
             }
         }
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
+        //reset the matrix;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 if(map.containsKey(i) || map.containsValue(j)){
-                    matrix[i][j]=0;
-                } 
+                    matrix[i][j] = 0;
+                }
             }
         }
     }
