9d69efd5e719fa35436a1107b35d82fcc8067f8d
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-07-05 02:33:12 +0000

    [LeetCode Sync] Runtime - 5 ms (70.92%), Memory - 43.2 MB (99.97%)

--- DSA/LEETCODE_AUTOMATED/1510-find-lucky-integer-in-an-array/solution.java
+++ DSA/LEETCODE_AUTOMATED/1510-find-lucky-integer-in-an-array/solution.java
@@ -1,13 +1,9 @@
 class Solution {
     public int findLucky(int[] arr) {
-        Map<Integer,Integer> map= new HashMap<>();
-        int res=-1;
-        for(int ele: arr) map.put(ele, map.getOrDefault(ele,0)+1);
-        for(int key: map.keySet()){
-            if(map.get(key)==key){
-                res=Math.max(res,key);
-            }
-        }  
+        int res = -1;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int ele: arr) map.put(ele, map.getOrDefault(ele, 0)+1);
+        for(int key: map.keySet()) if(map.get(key) == key) res = Math.max(res, key);
         return res;
     }
 }
