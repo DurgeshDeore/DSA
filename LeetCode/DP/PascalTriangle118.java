11e90e54c1ae180704ce8cfcb10c9b6db3b5e1c1
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-07-09 01:23:37 +0530

    Create PascalTriangle118.java

---
+++ LeetCode/DP/PascalTriangle118.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        temp.add(1);
+        res.add(temp);
+        for(int i=1; i<numRows; i++){
+            List<Integer> cur = new ArrayList<>();
+            for(int j=0; j<i+1; j++){
+                if(j==0 || j==i){
+                    cur.add(1);
+                }else{
+                    List<Integer> prev=res.get(i-1);
+                    cur.add(prev.get(j-1) + prev.get(j));
+                }
+                
+            }
+            res.add(cur);
+        }
+        return res;
+    }
+}
