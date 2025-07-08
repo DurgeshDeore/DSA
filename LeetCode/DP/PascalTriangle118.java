b34bb5cc5a5af3a6f3d6f809f11b41eefe296466
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-07-09 01:27:36 +0530

    Update PascalTriangle118.java

--- LeetCode/DP/PascalTriangle118.java
+++ LeetCode/DP/PascalTriangle118.java
@@ -1,10 +1,7 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> res = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        temp.add(1);
-        res.add(temp);
-        for(int i=1; i<numRows; i++){
+        for(int i=0; i<numRows; i++){
             List<Integer> cur = new ArrayList<>();
             for(int j=0; j<i+1; j++){
                 if(j==0 || j==i){
