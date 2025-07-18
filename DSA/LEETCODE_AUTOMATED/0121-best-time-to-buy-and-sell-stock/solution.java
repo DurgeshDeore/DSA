e1325149cff28fe52bb2ad96d35cd2af35e94857
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-07-18 13:26:39 +0000

    [LeetCode Sync] Runtime - 2 ms (45.90%), Memory - 61.7 MB (99.93%)

--- DSA/LEETCODE_AUTOMATED/0121-best-time-to-buy-and-sell-stock/solution.java
+++ DSA/LEETCODE_AUTOMATED/0121-best-time-to-buy-and-sell-stock/solution.java
@@ -1,11 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int maxProf=0, minValue=Integer.MAX_VALUE;
-        for(int i: prices){
-            minValue=Math.min(minValue,i);
-            int prof=i-minValue;
-            maxProf=Math.max(maxProf,prof);
+        int n=prices.length, res=0, min = prices[0];
+        // n
+        for(int i=1; i<n; i++){
+            res = Math.max(prices[i]-min, res);
+            min = Math.min(min, prices[i]);
         }
-        return maxProf;
+        // n^2;
+        // for(int i=0; i<n; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         res = Math.max(res, prices[j]-prices[i]);
+        //     }
+        // }
+        return res;
     }
 }
