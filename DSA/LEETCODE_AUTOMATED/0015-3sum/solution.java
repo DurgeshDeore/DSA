8877013580cadde9f52590a9abb36148f1f33b77
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-07-19 05:32:31 +0000

    [LeetCode Sync] Runtime - 26 ms (99.62%), Memory - 52.7 MB (99.96%)

--- DSA/LEETCODE_AUTOMATED/0015-3sum/solution.java
+++ DSA/LEETCODE_AUTOMATED/0015-3sum/solution.java
@@ -1,32 +1,27 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        int n =nums.length;
         List<List<Integer>> res = new ArrayList<>();
-        for(int i=0; i<n-2; i++){
+        int n = nums.length;
+        Arrays.sort(nums);
+        for(int i=0; i<n; i++){
             if(i>0 && nums[i] == nums[i-1]) continue;
-            int l=i+1, r=n-1;
-            while(l<r){
-                int sum = nums[i] + nums[l] + nums[r];
-                if(sum == 0){
-                    List<Integer> ans = new ArrayList<>();
-                    ans.add(nums[i]);
-                    ans.add(nums[l]);
-                    ans.add(nums[r]);
-                    res.add(ans);
-                    while(l<r && nums[l] == nums[l+1]){
-                        l++;
-                    }
-                    while(l<r && nums[r] == nums[r-1]){
-                        r--;
-                    }
-                    l++;
-                    r--;
-                }
-                else if(sum < 0){
-                    l++;
+            int j=i+1, k=n-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum > 0){
+                    k--;
+                }else if(sum < 0){
+                    j++;
                 }else{
-                    r--;
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
+                    res.add(temp);
+                    j++;
+                    k--;
+                    while(j<k && nums[j] == nums[j-1]) j++;
+                    while(j<k && nums[k] == nums[k+1]) k--;
                 }
             }
         }
