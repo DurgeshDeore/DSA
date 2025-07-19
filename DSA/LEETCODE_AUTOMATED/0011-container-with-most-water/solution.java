38aff6761da9f41c7c4bfaed5b0e02588cf14513
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-07-19 04:28:01 +0000

    [LeetCode Sync] Runtime - 5 ms (82.74%), Memory - 57.2 MB (99.92%)

--- DSA/LEETCODE_AUTOMATED/0011-container-with-most-water/solution.java
+++ DSA/LEETCODE_AUTOMATED/0011-container-with-most-water/solution.java
@@ -1,17 +1,15 @@
 class Solution {
     public int maxArea(int[] height) {
-        int n=height.length;
-        int l=0, r=n-1, ans=0;
+        int l=0, r=height.length-1, res=0;
         while(l<r){
-            int h = Math.min(height[l], height[r]);
-            int area = h*(r-l);
-            ans = Math.max(area,ans);
+            int curWater = Math.min(height[l], height[r])*(r-l);
+            res = Math.max(res, curWater);
             if(height[l] < height[r]){
                 l++;
             }else{
                 r--;
             }
         }
-        return ans;
+        return res;
     }
 }
