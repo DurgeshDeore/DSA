7876e04b07b41f82d0920800cb13cb9edbe8fdf6
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-08-01 09:15:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.3 MB (99.99%)

--- DSA/LEETCODE_AUTOMATED/0074-search-a-2d-matrix/solution.java
+++ DSA/LEETCODE_AUTOMATED/0074-search-a-2d-matrix/solution.java
@@ -1,13 +1,15 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int row=0, rows=matrix.length, cols=matrix[0].length, col=matrix[0].length-1 ;
-        while(row<rows && col >= 0){
-            if(matrix[row][col] == target){
+        int l=0, r=matrix.length* matrix[0].length-1, m=matrix[0].length;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            int val=matrix[mid/m][mid%m];
+            if(val==target){
                 return true;
-            }else if(matrix[row][col] > target ){
-                col--;
-            }else if(col >= 0){
-                row++;
+            }else if(val<target){
+                l=mid+1;
+            }else{
+                r=mid-1;
             }
         }
         return false;
