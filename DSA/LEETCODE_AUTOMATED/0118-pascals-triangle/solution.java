75e5a35b34d1e1c4d6fefbf4b62e380b9c367287
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-08-01 04:35:03 +0000

    [LeetCode Sync] Runtime - 1 ms (95.42%), Memory - 41.9 MB (99.98%)

--- DSA/LEETCODE_AUTOMATED/0118-pascals-triangle/solution.java
+++ DSA/LEETCODE_AUTOMATED/0118-pascals-triangle/solution.java
@@ -1,21 +1,33 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> res = new ArrayList<>();
-        // List<Integer> temp = new ArrayList<>();
-        // temp.add(1);
-        // res.add(temp);
+        // for(int i=0; i<numRows; i++){
+        //     List<Integer> cur = new ArrayList<>();
+        //     for(int j=0; j<i+1; j++){
+        //         if(j==0 || j==i){
+        //             cur.add(1);
+        //         }else{
+        //             List<Integer> prev=res.get(i-1);
+        //             cur.add(prev.get(j-1) + prev.get(j));
+        //         }
+        //     }
+        //     res.add(cur);
+        // }
+        
         for(int i=0; i<numRows; i++){
-            List<Integer> cur = new ArrayList<>();
+            // res.add(new ArrayList<>());
+            // if(i==0) res.get(0).add(1);
+            List<Integer> cur=new ArrayList<>();
             for(int j=0; j<i+1; j++){
                 if(j==0 || j==i){
                     cur.add(1);
                 }else{
-                    List<Integer> prev=res.get(i-1);
-                    cur.add(prev.get(j-1) + prev.get(j));
+                    cur.add(res.get(i-1).get(j-1)+res.get(i-1).get(j));
                 }
             }
             res.add(cur);
         }
         return res;
+        
     }
 }
