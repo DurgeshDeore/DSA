20c97a1f50edcb6fefe39fb065467ddd1a372740
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-12-01 10:09:39 +0000

    [LeetCode Sync] Runtime - 1 ms (56.19%), Memory - 48.4 MB (31.15%)

--- DSA/LEETCODE_AUTOMATED/0042-trapping-rain-water/solution.java
+++ DSA/LEETCODE_AUTOMATED/0042-trapping-rain-water/solution.java
@@ -1,14 +1,27 @@
 class Solution {
     public int trap(int[] height) {
-        int l=0, r=height.length-1, res=0, rmax=0, lmax=0;
-        while(l<r){
-            rmax=Math.max(rmax, height[r]);
-            lmax=Math.max(lmax, height[l]);
-            if(rmax<lmax){
-                res+=rmax-height[r--];
-            }else{
-                res+=lmax-height[l++];
-            }
+        int n = height.length, res=0;
+        int[] lMax = new int[n];
+        int[] rMax = new int[n];
+        // for(int i=0; i<n; i++){
+        //     if(i==0){
+        //         lMax[i] = height[i];
+        //         rMax[n-i-1] = height[n-i-1];
+        //     }else{
+        //         lMax[i] = Math.max(lMax[i-1], height[i]);
+        //         rMax[i] = Math.max(rMax[n-i-1], height[n-i]);
+        //     }
+        // }
+        lMax[0] = height[0];
+        for(int i=1; i<n; i++)
+            lMax[i] = Math.max(lMax[i-1], height[i]);
+
+        rMax[n-1] = height[n-1];
+        for(int i=n-2; i>=0; i--)
+            rMax[i] = Math.max(rMax[i+1], height[i]);
+
+        for(int i=0; i<n; i++){
+            res += Math.min(lMax[i], rMax[i]) - height[i];
         }
         return res;
     }
