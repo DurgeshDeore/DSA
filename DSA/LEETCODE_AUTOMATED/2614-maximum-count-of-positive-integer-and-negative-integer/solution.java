f50a54cbedaa6e2f44579b72f39439072d5f5656
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-12-28 04:12:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 47 MB (38.25%)

--- DSA/LEETCODE_AUTOMATED/2614-maximum-count-of-positive-integer-and-negative-integer/solution.java
+++ DSA/LEETCODE_AUTOMATED/2614-maximum-count-of-positive-integer-and-negative-integer/solution.java
@@ -1,13 +1,29 @@
 class Solution {
-    public int maximumCount(int[] nums) {
-        int posCnt=0, negCnt=0;
-        for(int ele: nums){
-            if(ele<0){
-                negCnt++;
-            }else if(ele>0){
-                posCnt++;
-            }
+    public int bsn(int[] nums){
+        int l =0, r=nums.length;
+        while(l<r){
+            int m = (r+l)/2;
+            if(nums[m] < 0)
+                l = m+1;
+            else
+                r = m;
         }
-        return Math.max(negCnt,posCnt);
+        return l;
+    }
+    public int bsp(int[] nums){
+        int l =0, r=nums.length;
+        while(l<r){
+            int m = (r+l)/2;
+            if(nums[m] <= 0)
+                l = m+1;
+            else
+                r = m;
+        }
+        return l;
+    }
+    public int maximumCount(int[] nums) {
+        int neg = bsn(nums), pos = bsp(nums), n = nums.length;
+        return Math.max(neg, n-pos);
+        // return pos;
     }
 }
