5c5a5cb635854abaae0f089f480ffd20fc04868f
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-01-01 04:03:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.9 MB (98.53%)

--- DSA/LEETCODE_AUTOMATED/0066-plus-one/solution.java
+++ DSA/LEETCODE_AUTOMATED/0066-plus-one/solution.java
@@ -1,24 +1,18 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int carry=1;
-        for(int i=digits.length-1; i>=0;i--){
-            digits[i]+=carry;
-            if(digits[i]>9){
-                digits[i]=0;
-                carry=1;
+        int n = digits.length, carry = 1;
+        for(int i=n-1; i>=0; i--){
+            digits[i] += carry;
+            if(digits[i] > 9){
+                digits[i] = 0;
             }else{
                 return digits;
             }
         }
-        if(carry==1){
-            int[] res= new int[digits.length+1];
-            //int i =0;
-            res[0]=carry;
-            // for(int ele:digits){
-            //     res[i++]=ele;
-            // }
-            return res;
+        int[] res = new int[n+1];;
+        if(carry == 1){
+            res[0] = 1;
         }
-        return digits;
+        return res;
     }
 }
