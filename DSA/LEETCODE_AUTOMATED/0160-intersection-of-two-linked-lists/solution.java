f5cc5022da787a6d9f66f943304d81159d61a2d9
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-01-01 04:13:32 +0000

    [LeetCode Sync] Runtime - 2 ms (47.39%), Memory - 53 MB (27.45%)

--- DSA/LEETCODE_AUTOMATED/0160-intersection-of-two-linked-lists/solution.java
+++ DSA/LEETCODE_AUTOMATED/0160-intersection-of-two-linked-lists/solution.java
@@ -10,19 +10,24 @@
  * }
  */
 public class Solution {
+    public int length(ListNode head){
+        int len = 0;
+        for(;head != null; head=head.next) len++;
+        return len;
+    }
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        HashMap<ListNode,Integer> map = new HashMap<>();
-        ListNode nullPtr=null;
-        ListNode temp=headA;
-        while(temp!=null){
-            map.put(temp,1);
-            temp=temp.next;
+        int n = length(headA), m = length(headB);
+        if(n > m){
+            for(int i=0, diff=n-m; i<diff; headA=headA.next)
+                i++;
+        }else if(m>n){
+            for(int i=0, diff=m-n; i<diff; headB=headB.next)
+                i++;
         }
-        temp=headB;
-        while(temp!=null){
-            if(map.containsKey(temp)) return temp;
-            temp=temp.next;
+        while(headA != headB){
+            headB=headB.next;
+            headA=headA.next;
         }
-        return nullPtr;
+        return headA;
     }
 }
