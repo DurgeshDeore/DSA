0f909239a712bf613055f0de4bd89de870eb9107
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-01-29 04:21:03 +0000

    [LeetCode Sync] Runtime - 2 ms (99.40%), Memory - 47.4 MB (11.46%)

--- DSA/LEETCODE_AUTOMATED/0001-two-sum/solution.java
+++ DSA/LEETCODE_AUTOMATED/0001-two-sum/solution.java
@@ -1,13 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length; i++){
-            int rem = target-nums[i];
-            if(map.containsKey(rem)){
-                return new int[]{i,map.get(rem)};
-            }
-            map.put(nums[i],i);
+        for(int i=0; i< nums.length; i++){
+            int n = nums[i];
+            if(map.containsKey(target - n)) return new int[]{map.get(target-n), i};
+            map.put(n,i);
         }
-        return new int[]{};
+        return new int[]{-1, -1};
     }
 }
