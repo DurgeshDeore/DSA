4e32a70466b292ff783ce716e576e3ed7b096241
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-02-19 01:58:16 +0000

    [LeetCode Sync] Runtime - 11 ms (54.41%), Memory - 46.4 MB (34.93%)

---
+++ DSA/LEETCODE_AUTOMATED/0696-count-binary-substrings/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int cur=1, prev = 0, res = 0;
+        for(int i=1; i<s.length(); i++){
+            if(s.charAt(i) == s.charAt(i-1)){
+                cur += 1;
+            }else{
+                res += Math.min(cur, prev);
+                prev = cur;
+                cur = 1;
+            }
+        }
+        res += Math.min(cur, prev);
+        return res;
+    }
+}
