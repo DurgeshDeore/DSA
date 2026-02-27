dc77fb8911dc7827e67de2d512c4e49993deef80
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-02-27 12:13:39 +0000

    [LeetCode Sync] Runtime - 14 ms (69.83%), Memory - 47.5 MB (81.73%)

--- DSA/LEETCODE_AUTOMATED/0347-top-k-frequent-elements/solution.java
+++ DSA/LEETCODE_AUTOMATED/0347-top-k-frequent-elements/solution.java
@@ -1,20 +1,25 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        PriorityQueue<int[]> pq= new PriorityQueue<>((a,b)->b[1]==a[1]? b[0]-a[0]: b[1]-a[1]);
-        for(int ele: nums){
-            map.put(ele,map.getOrDefault(ele,0)+1);
-            // int freq=map.getOrDefault(ele,0);
-            // pq.add(new int[]{ele,freq});
-        }
-        for(int key: map.keySet()){
-            pq.add(new int[]{key,map.get(key)});
-        }
         int[] res = new int[k];
-        int i=0;
-        while(i<k){
-            res[i++]=pq.poll()[0];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i: nums)
+            map.put(i, map.getOrDefault(i, 0)+1);
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b)->{
+            if(b[1] == a[1])
+                return a[0] - b[0];
+            return a[1] - b[1];
+        });
+
+        for(int key: map.keySet()){
+            pq.add(new int[]{key, map.get(key)});
+            if(pq.size() > k) pq.poll();
         }
+
+        int i=0;
+        while(i<k)
+            res[i++] = pq.poll()[0];
+        
         return res;
     }
 }
