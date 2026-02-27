64d37af404a2eb148fd791cbd36712cf7cbe1238
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-02-27 07:32:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.3 MB (35.38%)

--- DSA/LEETCODE_AUTOMATED/1603-running-sum-of-1d-array/solution.java
+++ DSA/LEETCODE_AUTOMATED/1603-running-sum-of-1d-array/solution.java
@@ -1,11 +1,10 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        int[] sum = new int[nums.length];
-        int currSum=0;
+        int sum = 0;
         for(int i=0; i<nums.length; i++){
-            currSum += nums[i];
-            sum[i] = currSum;
+            sum += nums[i];
+            nums[i] = sum;
         }
-        return sum;
+        return nums;
     }
 }
