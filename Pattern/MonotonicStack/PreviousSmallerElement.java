89b8ea78d991f6a43486572ecec0d00110b050ff
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-03-12 11:23:12 +0530

    Create PreviousSmallerElement.java
    GFG Medium
    TC O(n)
    SC O(n)

---
+++ Pattern/MonotonicStack/PreviousSmallerElement.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static ArrayList<Integer> prevSmaller(int[] arr) {
+        int n = arr.length;
+        ArrayList<Integer> res = new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
+        
+        for(int i=0; i<n; i++){
+            while(!st.isEmpty() && st.peek() >= arr[i])
+                st.pop();
+            if(st.isEmpty())
+                res.add(-1);
+            else
+                res.add(st.peek());
+            st.push(arr[i]);
+        }
+        
+        return res;
+    }
+}
