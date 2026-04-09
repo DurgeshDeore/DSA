9c69ca432bed91f767361a6a4971cc0e2548858c
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-04-09 14:03:31 +0000

    [LeetCode Sync] Runtime - 13 ms (45.06%), Memory - 56.9 MB (39.01%)

---
+++ DSA/LEETCODE_AUTOMATED/0518-coin-change-ii/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        // if(amount == 0) return 0;
+        int n=coins.length;
+        int[][] dp = new int[n+1][amount+1];
+        for(int i=0; i<n+1; i++){
+            dp[i][0] = 1;
+        }
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<amount+1; j++){
+                int val = coins[i-1];
+                dp[i][j] = dp[i-1][j];
+                if(val <= j)
+                    dp[i][j] += dp[i][j-val];
+            }
+        }
+        return dp[n][amount];
+    }
+}
