d6ff4908867939118e82853e79b63126dbbd3a4a
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-04-17 14:22:14 +0000

    [LeetCode Sync] Runtime - 90 ms (21.04%), Memory - 48.5 MB (41.04%)

---
+++ DSA/LEETCODE_AUTOMATED/2470-removing-stars-from-a-string/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String removeStars(String s) {
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> st = new Stack<>();
+        for(char c: s.toCharArray()){
+            if(c == '*'){
+                if(!st.isEmpty()) st.pop();
+            }else{
+                st.push(c);
+            }
+        }
+        while(!st.isEmpty()) sb.append(st.pop());
+        return sb.reverse().toString();
+    }
+}
