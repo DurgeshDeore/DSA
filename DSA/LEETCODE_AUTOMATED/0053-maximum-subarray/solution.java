fd2f94942e8c0ff646d95825d242b782818a747c
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-04-20 12:41:05 +0000

    [LeetCode Sync] Runtime - 1 ms (99.93%), Memory - 77 MB (91.97%)

--- DSA/LEETCODE_AUTOMATED/0053-maximum-subarray/solution.java
+++ DSA/LEETCODE_AUTOMATED/0053-maximum-subarray/solution.java
@@ -1,20 +1,9 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // int res=0, i=0, j=0, n=nums.length;
-        // while(i<n && j<n){
-        //     if(j<n){
-        //         res=Math.max(res, res+nums[j]);
-        //         j++;
-        //     }else if(j>=n){
-        //         res=Math.max(res, res-nums[i]);
-        //         i++;
-        //     }
-        // }
-        // return res;
-        int curSum =0, res = nums[0];
-        for(int i=0; i<nums.length; i++){
-            curSum = Math.max(nums[i],curSum+nums[i]);
-            res = Math.max(res, curSum);
+        int res=nums[0], cur=nums[0];
+        for(int i=1; i<nums.length; i++){
+            cur = Math.max(nums[i], cur+nums[i]);
+            res = Math.max(cur, res);
         }
         return res;
     }
