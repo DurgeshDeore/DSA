a855b9dd4ae307baefc64e13a3bfd3fcce3f37b4
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-04-20 12:56:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.2 MB (100.00%)

--- DSA/LEETCODE_AUTOMATED/0104-maximum-depth-of-binary-tree/solution.java
+++ DSA/LEETCODE_AUTOMATED/0104-maximum-depth-of-binary-tree/solution.java
@@ -13,11 +13,15 @@
  *     }
  * }
  */
+
 class Solution {
-    public int maxDepth(TreeNode root) {
+    public int helper(TreeNode root){
         if(root == null) return 0;
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        return Math.max(left, right)+1;
+        int l = helper(root.left);
+        int r = helper(root.right);
+        return Math.max(l, r)+1;
+    }
+    public int maxDepth(TreeNode root) {
+        return helper(root);
     }
 }
