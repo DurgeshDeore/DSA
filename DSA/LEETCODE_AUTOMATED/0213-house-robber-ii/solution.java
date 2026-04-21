59e0392998bafe37191442b71e6baf96425f2e36
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-04-21 05:41:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.7 MB (60.06%)

--- DSA/LEETCODE_AUTOMATED/0213-house-robber-ii/solution.java
+++ DSA/LEETCODE_AUTOMATED/0213-house-robber-ii/solution.java
@@ -1,23 +1,18 @@
 class Solution {
-    public int houseRobber(int[] nums){
-        int n = nums.length, prev1=0, cur=0, prev2=nums[0];
-        for(int i=1; i<n; i++){
-            cur = Math.max(nums[i]+prev1, prev2);
-            prev1 = prev2; 
-            prev2 = cur;
+    public int helper(int[] nums, int st, int end){
+        int prev2 = nums[st];
+        int prev1 = Math.max(nums[st], nums[st+1]);
+        for(int i=st+2; i<end; i++){
+            int cur = Math.max(prev1, prev2+nums[i]);
+            prev2 = prev1; 
+            prev1 = cur;
         }
-        return cur;
+        return prev1;
     }
     public int rob(int[] nums) {
-        int n=nums.length, j=0, k=0;
-        if(n == 1) return nums[n-1];
-        if(n == 2) return Math.max(nums[0],nums[1]);
-        int[] nums1= new int[n-1];
-        int[] nums2= new int[n-1];
-        for(int i=0; i<n; i++){
-            if(i!=0) nums1[j++]=nums[i];
-            if(i!=n-1) nums2[k++]=nums[i];
-        }
-        return Math.max(houseRobber(nums1), houseRobber(nums2));
+        int n=nums.length;
+        if(n == 1) return nums[0];
+        else if(n == 2) return Math.max(nums[0], nums[1]);
+        return Math.max(helper(nums, 0, n-1), helper(nums, 1, n));
     }
 }
