b2c6d1b39f28b446f5ae36d38fb8220d57f8b612
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-04-24 03:53:46 +0000

    [LeetCode Sync] Runtime - 8 ms (15.58%), Memory - 46.9 MB (7.86%)

--- DSA/LEETCODE_AUTOMATED/0120-triangle/solution.java
+++ DSA/LEETCODE_AUTOMATED/0120-triangle/solution.java
@@ -1,16 +1,20 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n=triangle.size();
-        // int[] dp = new int[n];
-        // for(int i=0; i<n; i++)
-        //     dp[i] = triangle.get(n-1).get(i);
-        for(int i=n-2; i>=0; i--){
-            for(int j=0; j<=i; j++){
-                // dp[j] = triangle.get(i).get(j) + Math.min(dp[j],dp[j+1]);
-                int val = triangle.get(i).get(j) + Math.min(triangle.get(i+1).get(j), triangle.get(i+1).get(j+1));
-                triangle.get(i).set(j, val);
+        for(int i=1; i<n; i++){
+            for(int j=0; j<i+1; j++){
+                int cur = triangle.get(i).get(j);
+                if(j==0)
+                    cur += triangle.get(i-1).get(j);
+                else if(j == i)
+                    cur += triangle.get(i-1).get(j-1);
+                else if(i > 0 && j<i)
+                    cur += Math.min(triangle.get(i-1).get(j), triangle.get(i-1).get(j-1));
+                triangle.get(i).set(j, cur);
             }
         }
-        return triangle.get(0).get(0);
+        int res = Integer.MAX_VALUE;
+        for(int i=0; i<n; i++) res = Math.min(res, triangle.get(n-1).get(i));
+        return res;
     }
 }
