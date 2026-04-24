1eab5501e7df4141283fff14a55e6fb47314e870
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-04-24 03:31:53 +0000

    [LeetCode Sync] Runtime - 4 ms (59.93%), Memory - 50.6 MB (10.67%)

--- DSA/LEETCODE_AUTOMATED/0064-minimum-path-sum/solution.java
+++ DSA/LEETCODE_AUTOMATED/0064-minimum-path-sum/solution.java
@@ -1,21 +1,26 @@
 class Solution {
+    public int helper(int[][] grid, int i, int j){ //memo
+        int m= grid.length, n=grid[0].length;
+        if(i >= m || j >= n || i<0 || j <0) return 0;
+        if(i == m-1 && j == n-1) return grid[m-1][n-1];
+        int r = grid[i][j] + helper(grid, i, j+1);
+        int d = grid[i][j] + helper(grid, i+1, j);
+        return Math.min(r, d);
+    }
     public int minPathSum(int[][] grid) {
-        int n=grid.length, m=grid[0].length;
-
-        // for(int i=1; i<n; i++) grid[i][0] += grid[i-1][0];
-        // for(int j=1; j<n; j++) grid[0][j] += grid[0][j-1];
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(i == 0 && j > 0)
-                    grid[i][j] += grid[i][j-1];
-                else if(j == 0 && i > 0)
-                    grid[i][j] += grid[i-1][j];
-                else if(j > 0 && i > 0)
+        int m= grid.length, n=grid[0].length;
+        // int[][] dp = new int[m+1][n+1];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i>0 && j>0)
                     grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
+                else if(i > 0)
+                    grid[i][j] += grid[i-1][j];
+                else if(j > 0)
+                    grid[i][j] += grid[i][j-1];
             }
         }
-
-        return grid[n-1][m-1];
+        // return helper(grid, 0, 0);
+        return grid[m-1][n-1];
     }
 }
