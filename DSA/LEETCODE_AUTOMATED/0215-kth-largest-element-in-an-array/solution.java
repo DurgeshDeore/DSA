9fc1c496b3907c8c8d6827adf612abcba11d995c
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-05-01 15:03:08 +0000

    [LeetCode Sync] Runtime - 67 ms (59.56%), Memory - 68.8 MB (99.95%)

--- DSA/LEETCODE_AUTOMATED/0215-kth-largest-element-in-an-array/solution.java
+++ DSA/LEETCODE_AUTOMATED/0215-kth-largest-element-in-an-array/solution.java
@@ -1,12 +1,10 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        // if(k>nums.length) return -1;
-        // PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
-        // for(int ele: nums) pq.add(ele);
-        // for(int i=0;i<k-1;i++) pq.poll();
-        // return pq.poll();
-        int n=nums.length;
-        Arrays.sort(nums);
-        return nums[n-k];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i: nums){
+            pq.add(i);  
+            if(pq.size() > k) pq.poll();
+        }
+        return pq.peek();
     }
 }
