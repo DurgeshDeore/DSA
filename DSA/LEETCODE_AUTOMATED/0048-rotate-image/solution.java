0deed086aa87b37ed3ed76fc05c2e62009cc2314
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-05-04 02:40:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.8 MB (52.69%)

--- DSA/LEETCODE_AUTOMATED/0048-rotate-image/solution.java
+++ DSA/LEETCODE_AUTOMATED/0048-rotate-image/solution.java
@@ -1,30 +1,24 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int n = matrix.length;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
+        int n=matrix.length;
+        //inverse;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
-        //reves
-
-        for (int i = 0; i < n; i++) {
-            int left = 0;
-            int right = n - 1;
-
-            while (left < right) {
-                int temp = matrix[i][left];
-                matrix[i][left] = matrix[i][right];
-                matrix[i][right] = temp;
-
-                left++;
-                right--;
+        //reverse;
+        for(int i=0; i<n; i++){
+            int l=0, r=n-1;
+            while(l<r){
+                int temp = matrix[i][l];
+                matrix[i][l] = matrix[i][r];
+                matrix[i][r] = temp;
+                l++;
+                r--;
             }
         }
-
-  
     }
 }
