fc0dd96497e36f65fecbdb3a390e2ecfbe2a5ce0
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-05-04 13:43:49 +0000

    [LeetCode Sync] Runtime - 8 ms (83.66%), Memory - 46 MB (95.83%)

--- DSA/LEETCODE_AUTOMATED/0003-longest-substring-without-repeating-characters/solution.java
+++ DSA/LEETCODE_AUTOMATED/0003-longest-substring-without-repeating-characters/solution.java
@@ -1,16 +1,21 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int max=0, start=0;
-        HashSet<Character> set = new HashSet<>();
-        for(int end=0; end<s.length(); end++){
-            char ch = s.charAt(end);
-            while(set.contains(ch)){
-                set.remove(s.charAt(start));
-                start++;
+        int res =0, l=0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int r=0; r<s.length(); r++){
+            map.put(s.charAt(r), map.getOrDefault(s.charAt(r), 0)+1);
+            while(r-l+1 > map.size() && l<r){
+                map.put(s.charAt(l), map.getOrDefault(s.charAt(l), 0)-1);
+                if(map.get(s.charAt(l)) == 0) map.remove(s.charAt(l));
+                l++;
             }
-            set.add(ch);
-            max = Math.max(max, end-start+1);
+            // while(map.get(s.charAt(r)) ==  map.size() && l<r){
+            //     map.put(s.charAt(l), map.getOrDefault(s.charAt(l), 0)+1);
+            //     if(map.get(s.charAt(l)) == 0) map.remove(s.charAt(l));
+            //     l++;
+            // }
+            if(r-l+1 == map.size()) res = Math.max(res, (r-l)+1);
         }
-        return max;
+        return res;
     }
 }
