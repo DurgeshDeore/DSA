0d73063c5cde23a3aa8172c2093596d53eac1df3
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-05-05 06:20:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.5 MB (42.84%)

--- DSA/LEETCODE_AUTOMATED/0027-remove-element/solution.java
+++ DSA/LEETCODE_AUTOMATED/0027-remove-element/solution.java
@@ -1,23 +1,14 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        //HashMap<Integer,Integer> map = new HashMap<>();
-        ArrayList<Integer> arr = new ArrayList<>();
-        for(int ele: nums){
-            if(ele != val){
-                //map.put(ele,map.getOrDefault(ele,0)+1);
-                arr.add(ele);
+        int n=nums.length, j=0, i=0;
+        while(i<n){
+            while( i<n && nums[i] != val){
+                nums[j] = nums[i];
+                j++;
+                i++;
             }
+            i++;
         }
-        int i=0;
-        // for(int ele: map.keySet()){
-        //     nums[i++] = ele;
-        // }
-        for(int ele: arr){
-            nums[i++] = ele;
-        }
-        // while(i<nums.length){
-        //     nums[i++]=0;
-        // }
-        return arr.size();
+        return j;
     }
 }
