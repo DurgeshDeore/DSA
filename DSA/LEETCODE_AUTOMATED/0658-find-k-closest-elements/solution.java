57a35fb880bd6fd3a4878319b5bf33a3c1aad76b
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-05-05 03:21:37 +0000

    [LeetCode Sync] Runtime - 5 ms (56.53%), Memory - 48 MB (36.84%)

--- DSA/LEETCODE_AUTOMATED/0658-find-k-closest-elements/solution.java
+++ DSA/LEETCODE_AUTOMATED/0658-find-k-closest-elements/solution.java
@@ -1,27 +1,17 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int i=0;
-        int[] temp = new int[k];
         List<Integer> res = new ArrayList<>();
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> {
-            if(a[1]==b[1])
-                return a[0]-b[0];
-            return a[1]-b[1];
-        });
-
-        for(int a: arr)
-            pq.add(new int[]{a, Math.abs(x-a)});
-
-        while(i<k){
-            // res.add(pq.poll()[0]);
-            temp[i++] = pq.poll()[0];
-            // i++;
+        int l=0, r=arr.length-1, indx=-1;
+        while((r-l+1) > k){
+            if(Math.abs(arr[l] - x) > Math.abs(arr[r] - x)){
+                l++;
+            }else{
+                r--;
+            } 
         }
-
-        Arrays.sort(temp);
-        for(int t: temp)
-            res.add(t);
-        
+        for(int i=l; i<l+k; i++){
+            res.add(arr[i]);
+        }
         return res;
     }
 }
