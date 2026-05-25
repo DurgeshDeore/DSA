fe26a8a8ec7f520103142a13cd6182eb37c971e5
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-05-25 02:19:52 +0000

    [LeetCode Sync] Runtime - 2 ms (89.65%), Memory - 48.2 MB (8.74%)

--- DSA/LEETCODE_AUTOMATED/0055-jump-game/solution.java
+++ DSA/LEETCODE_AUTOMATED/0055-jump-game/solution.java
@@ -1,10 +1,20 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int max=0;
-        for(int i=0; i<nums.length; i++){
-            if(i>max) return false;
-            max=Math.max(nums[i]+i, max);
+        int n=nums.length;
+        if(n==1) return true;
+        // boolean[] dp = new boolean[n];
+        // dp[0] = true;
+        // for(int i=0; i<n; i++){
+        //     if(dp[i]){
+        //         for(int j=i; j<Math.min(i+nums[i]+1, n); j++) dp[j] = true;
+        //     }
+        // }
+        int max=nums[0];
+        for(int i=0; i<n; i++){
+            if(i > max) return false;
+            max = Math.max(nums[i]+i, max);
         }
+        // return dp[n-1];
         return true;
     }
 }
