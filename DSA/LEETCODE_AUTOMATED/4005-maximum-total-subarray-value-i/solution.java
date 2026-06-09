c697ef62f8472f0d6b46e93df1de2fd8d68235ca
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-06-09 03:19:05 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 62.5 MB (15.31%)

---
+++ DSA/LEETCODE_AUTOMATED/4005-maximum-total-subarray-value-i/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        long min = Long.MAX_VALUE, max = Long.MIN_VALUE;
+        for(int i: nums){
+            min = Math.min(min, i);
+            max = Math.max(max, i);
+        }
+        return (max-min)*k;
+    }
+}
