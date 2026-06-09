6c77d661c06f136f724f0c9015ddd0e38d811a3d
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-06-09 05:25:42 +0000

    [LeetCode Sync] Runtime - 7 ms (21.70%), Memory - 45.5 MB (41.24%)

---
+++ DSA/LEETCODE_AUTOMATED/0137-single-number-ii/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n=nums.length;
+
+        // approach 1: use HashMap
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i: nums) map.put(i, map.getOrDefault(i, 0)+1);
+        for(int k: map.keySet()){
+            if(map.get(k) == 1) return k;
+        }
+        return -1;
+
+        // approach 2: use Soring
+        
+        // approach 3: use Bit manupulation
+    }
+}
