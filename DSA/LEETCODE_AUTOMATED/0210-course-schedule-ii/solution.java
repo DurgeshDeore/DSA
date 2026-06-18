3ca05b3451c474ce1efff4e5d4f3ab97c0628f6d
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-06-18 16:57:08 +0000

    [LeetCode Sync] Runtime - 4 ms (93.22%), Memory - 46.8 MB (67.15%)

---
+++ DSA/LEETCODE_AUTOMATED/0210-course-schedule-ii/solution.java
@@ -0,0 +1,61 @@
+class Solution {
+    public boolean[] isVisited;
+    public boolean[] isParentNode;
+    public Stack<Integer> stack;
+    public ArrayList<ArrayList<Integer>> graph;
+
+    public boolean isCycle(int node){ // cycle detection
+        isVisited[node] = true;
+        isParentNode[node] = true;
+        for(int neg: graph.get(node)){
+            if(!isVisited[neg]){
+                if(isCycle(neg)) return true;
+            }else if(isParentNode[neg]){
+                return true;
+            }
+        }
+        isParentNode[node] = false;
+        return false;
+    }
+    public void dfs(int node){ // topological sort for order off execution
+        isVisited[node] = true;
+        for(int neg: graph.get(node)){
+            if(!isVisited[neg]) dfs(neg);
+        }
+        stack.push(node);
+    }
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        isVisited = new boolean[numCourses];
+        isParentNode = new boolean[numCourses];
+        stack = new Stack<>();
+        graph = new ArrayList<>();
+
+        //create graph
+        for(int i=0; i<numCourses; i++) graph.add(new ArrayList<>());
+
+        for(int[] prerequisite: prerequisites){
+            graph.get(prerequisite[1]).add(prerequisite[0]);
+        }
+
+        // find the cycle 
+        for(int i=0; i<numCourses; i++){
+            if(!isVisited[i]){
+                if(isCycle(i)) return new int[]{};;
+            }
+        }
+
+        isVisited = new boolean[numCourses]; // reset vis array
+        //traaverse
+        for(int i=0; i<numCourses; i++){
+            if(!isVisited[i]){
+                dfs(i);
+            }
+        }
+
+        int[] res = new int[stack.size()];
+        for(int i=0; i<numCourses; i++){
+            res[i] = stack.pop();
+        }
+        return res;
+    }
+}
