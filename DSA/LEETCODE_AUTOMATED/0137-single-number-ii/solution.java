3211ccca72fe35135e8e5fec6d61fc9a21ed4048
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-07-29 16:36:40 +0000

    [LeetCode Sync] Runtime - 1 ms (93.18%), Memory - 45 MB (86.21%)

--- DSA/LEETCODE_AUTOMATED/0137-single-number-ii/solution.java
+++ DSA/LEETCODE_AUTOMATED/0137-single-number-ii/solution.java
@@ -3,13 +3,18 @@ class Solution {
         int n=nums.length;
 
         // approach 1: use HashMap
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i: nums) map.put(i, map.getOrDefault(i, 0)+1);
-        for(int k: map.keySet()){
-            if(map.get(k) == 1) return k;
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for(int i: nums) map.put(i, map.getOrDefault(i, 0)+1);
+        // for(int k: map.keySet()){
+        //     if(map.get(k) == 1) return k;
+        // }
+        // return -1;
+        int ones = 0, twos = 0;
+        for(int num: nums){
+            ones = (ones^num) & ~twos;
+            twos = (twos^num) & ~ones;
         }
-        return -1;
-
+        return ones;
         // approach 2: use Soring
         
         // approach 3: use Bit manupulation
