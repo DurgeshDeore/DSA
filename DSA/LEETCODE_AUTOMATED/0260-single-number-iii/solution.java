ab878b63f71b2002e2795023ed85c892b152f669
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-07-29 16:41:55 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 48 MB (39.79%)

--- DSA/LEETCODE_AUTOMATED/0260-single-number-iii/solution.java
+++ DSA/LEETCODE_AUTOMATED/0260-single-number-iii/solution.java
@@ -4,18 +4,22 @@ class Solution {
         // if(n == 2) return nums;
         int[] res = new int[2];
         // approach 1: use HashMap
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i: nums) map.put(i, map.getOrDefault(i, 0)+1);
-        for(int k: map.keySet()){
-            if(map.get(k) == 1) res[j++]=k;
-            if(j == 2) break;
-        }
-        return res;
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for(int i: nums) map.put(i, map.getOrDefault(i, 0)+1);
+        // for(int k: map.keySet()){
+        //     if(map.get(k) == 1) res[j++]=k;
+        //     if(j == 2) break;
+        // }
+        // return res;
 
         // approach 1: use HashMap
-        // int xor = 0;
-        // if(n == 2) return nums;
-        // for(int i: nums) xor ^= i;
-        // return new int[]{xor};
+        int xor = 0;
+        for(int num: nums) xor ^= num;
+        int mask = xor & -xor;
+        for(int num: nums){
+            if((num&mask) != 0) res[0] ^= num;
+            else res[1] ^=num; 
+        }
+        return res;
     }
 }
