9d915d476dcf37881e316f39061766234abec64d
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-08-02 02:13:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.2 MB (41.13%)

--- DSA/LEETCODE_AUTOMATED/0909-stone-game/solution.java
+++ DSA/LEETCODE_AUTOMATED/0909-stone-game/solution.java
@@ -1,5 +1,24 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
+        // approach 1: use simulation
+        // int l=0, r=piles.length-1, scoreA = 0, scoreB = 0;
+        // while(l<r){
+        //     if(piles[l] >= piles[r]){
+        //         scoreA += piles[l];
+        //         l+=1;
+        //     }else{
+        //         scoreA += piles[r];
+        //         r-=1;
+        //     }
+        //     if(piles[l] >= piles[r]){
+        //         scoreB += piles[l];
+        //         l+=1;
+        //     }else{
+        //         scoreB += piles[r];
+        //         r-=1;
+        //     }
+        // }
+        // return scoreA >= scoreB;
         return true;
     }
 }
