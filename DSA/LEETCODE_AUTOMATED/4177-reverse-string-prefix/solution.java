7aa47ea63cea1abbe652d92e91ad1a0b4f899e6b
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-08-12 13:17:01 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 44.6 MB (66.22%)

---
+++ DSA/LEETCODE_AUTOMATED/4177-reverse-string-prefix/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String reversePrefix(String s, int k) {
+        int n=s.length();
+        StringBuilder sb = new StringBuilder();
+        for(int i=k-1; i>=0; i--) sb.append(s.charAt(i));
+        for(int i=k; i<n; i++) sb.append(s.charAt(i));
+        return sb.toString();
+    }
+}
