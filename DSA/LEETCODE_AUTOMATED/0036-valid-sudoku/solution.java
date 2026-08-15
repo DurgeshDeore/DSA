72f38765b94fde5550ce81f101d3fec523ce2fbe
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-08-15 10:47:24 +0000

    [LeetCode Sync] Runtime - 1 ms (99.93%), Memory - 46.2 MB (91.17%)

--- DSA/LEETCODE_AUTOMATED/0036-valid-sudoku/solution.java
+++ DSA/LEETCODE_AUTOMATED/0036-valid-sudoku/solution.java
@@ -1,24 +1,23 @@
 class Solution {
-    public boolean isValidSudoku(char[][] board) {
-        HashSet<Character>[] rows = new HashSet[9];
-        HashSet<Character>[] cols = new HashSet[9];
-        HashSet<Character>[] boxes = new HashSet[9];
-        for(int i=0; i<9; i++){
-            rows[i] = new HashSet<>();
-            cols[i] = new HashSet<>();
-            boxes[i] = new HashSet<>();
+    public boolean isValid(char[][] board, int r, int c, char num){
+        for (int i = 0; i < 9; i++) {
+            if(board[r][i] == num && (i!=c)) return false;
+            if(board[i][c] == num && (i!=r)) return false;
         }
+        int bR = (r/3)*3, bC = (c/3)*3;
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                if((bR+i) == r && (bC+j) == c) continue;
+                if(board[bR+i][bC+j] == num) return false;
+            }
+        }
+        return true;        
+    }
+    public boolean isValidSudoku(char[][] board) {
         for(int i=0; i<9; i++){
             for(int j=0; j<9; j++){
-                char ch = board[i][j];
-                if(ch == '.') continue;
-                int boxIndx = (i/3)*3 + (j/3);
-                if(rows[i].contains(ch) || cols[j].contains(ch) || boxes[boxIndx].contains(ch)){
-                    return false;
-                }
-                rows[i].add(ch);
-                cols[j].add(ch);
-                boxes[boxIndx].add(ch);
+                if(board[i][j] == '.') continue;
+                if(!(isValid(board, i, j, board[i][j]))) return false;
             }
         }
         return true;
