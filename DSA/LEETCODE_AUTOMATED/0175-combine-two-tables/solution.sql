6fd2e49de48adf3ce1486dd355ce503291a6289c
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-08-24 07:48:26 +0000

    [LeetCode Sync] Runtime - 505 ms (28.42%), Memory - 0.0B (100.00%)

--- DSA/LEETCODE_AUTOMATED/0175-combine-two-tables/solution.sql
+++ DSA/LEETCODE_AUTOMATED/0175-combine-two-tables/solution.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
-select p.firstName, p.lastName, a.city, a.state 
-    from Person p 
-    left outer join 
-    Address a
-    on p.personId = a.personId;
+SELECT Person.firstName, Person.lastName, Address.city, Address.state 
+FROM Person 
+LEFT JOIN Address 
+    ON Person.personId = Address.personId;
 
