2af03c5874c29bbfa5e040bf9a7a3ef50dfa7541
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-08-30 02:27:23 +0000

    [LeetCode Sync] Runtime - 332 ms (93.01%), Memory - 0.0B (100.00%)

---
+++ DSA/LEETCODE_AUTOMATED/2127-employees-whose-manager-left-the-company/solution.sql
@@ -0,0 +1,8 @@
+SELECT e.employee_id
+FROM Employees e
+WHERE e.salary < 30000
+    AND e.manager_id NOT IN (
+        SELECT m.employee_id
+        FROM Employees m
+    )
+ORDER BY e.employee_id ASC;
