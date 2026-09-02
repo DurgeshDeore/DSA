00d297c864b8f19261ec9ee428afdc37ccf585d8
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-09-02 04:56:00 +0000

    [LeetCode Sync] Runtime - 1 ms (92.75%), Memory - 46.4 MB (66.03%)

---
+++ DSA/LEETCODE_AUTOMATED/4354-unique-middle-element/solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isMiddleElementUnique(int[] nums) {
+        int n = nums.length;
+        int[] freq = new int[101];
+        for(int num: nums) freq[num] += 1;
+        return freq[nums[n/2]] == 1;
+    }
+}
