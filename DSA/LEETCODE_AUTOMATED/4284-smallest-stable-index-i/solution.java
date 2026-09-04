4f2b6f530d5fba0f992cfbf8d8f7fd2a619f1f3c
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2026-09-04 02:43:01 +0000

    [LeetCode Sync] Runtime - 1 ms (99.37%), Memory - 46.2 MB (69.97%)

--- DSA/LEETCODE_AUTOMATED/4284-smallest-stable-index-i/solution.java
+++ DSA/LEETCODE_AUTOMATED/4284-smallest-stable-index-i/solution.java
@@ -1,22 +1,22 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        int n=nums.length, minE=Integer.MAX_VALUE, maxE=Integer.MIN_VALUE;
-        int[] max = new int[n];
-        int[] min = new int[n];
+        int n = nums.length;
+        int[] minArr = new int[n];
+        int[] maxArr = new int[n];
         
+        for(int i=n-1; i>=0; i--) {
+            if(i == n-1) minArr[i] = nums[i];
+            else minArr[i] = Math.min(minArr[i+1], nums[i]);
+        }
+        for(int i=0; i<n; i++) {
+            if(i == 0) maxArr[i] = nums[i];
+            else maxArr[i] = Math.max(maxArr[i-1], nums[i]);
+        }
+
         for(int i=0; i<n; i++){
-            maxE = Math.max(maxE, nums[i]);
-            max[i] = maxE;
+            if(maxArr[i]-minArr[i] <= k) return i; 
         }
         
-        for(int i=n-1; i>=0; i--){
-            minE = Math.min(minE, nums[i]);
-            min[i] = minE;
-        }
-        for(int i=0; i<n; i++){
-            if((max[i] - min[i]) <= k)
-                return i;
-        }
         return -1;
     }
 }
